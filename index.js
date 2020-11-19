9d52da918c081e24d6adaa17f061fff4c119bb48
Brandon Pina <brandonpi265@gmail.com>
2020-11-19 12:14:49 -0600

    Change how users are informed about functionality

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const tmi = require("tmi.js");
 require("dotenv").config();
 const hue = require("hue-node");
 const axios = require("axios").default;
-var convert = require('color-convert');
+var convert = require("color-convert");
 
 const opts = {
   identity: {
@@ -18,26 +18,53 @@ client.connect();
 client.on("message", onMessageHandler);
 client.on("connected", onConnectedHandler);
 
+var usersList = [];
 
 function onMessageHandler(target, context, msg, self) {
-    console.log(target);
   var message = { target, context, msg, self };
+
   console.log(context.username + ": " + msg);
 
   if (context.username != process.env.TWITCH_NAME) {
     //client.say(target,`Hi ${context.username}`)
   }
 
-  console.log(msg.substr(0,6));
-  if (msg.substr(0,7).includes("!lights")) {
+  if (
+    !usersList.includes(context.username) &&
+    context.username != process.env.TWITCH_NAME
+  ) {
+    usersList.push(context.username);
+    
+    var index = usersList.length - 1;
 
-     color = msg.substr(8,msg.length);
-     console.log(color);
-     var huey = convert.keyword.hsl(color);
-     console.log(huey);
+    console.log(usersList);
+
+    client.say(target,`Hey, ${context.username}, To change the color of ${process.env.CHANNEL_WATCH}'s lights, just type !lights then the color you want them to change to. Go ahead, give it a try!`);
+
+    // Remove the user from the list after an hour
+    setTimeout(()=>{
+      usersList.forEach((username,i)=>{
+        if (username == context.username) {
+          usersList = usersList.splice(i, i);
+        }
+        //console.log(usersList);
+        
+      })
+    },36000000)
+    
+
+   
+  }
+
+  console.log(msg.substr(0, 6));
+  if (msg.substr(0, 7).includes("!lights")) {
+    color = msg.substr(8, msg.length);
+
+    var huey = convert.keyword.hsl(color);
+    
 
     setLights(huey[0], 255, 255, target);
-    client.say(target,`Lights changed to ${color}`);
+    client.say(target, `Lights changed to ${color}`);
   }
 }
 
@@ -45,17 +72,17 @@ function onConnectedHandler(addr, port) {
   console.log("Connected to channel");
   target = "#withenex";
 
-  setInterval(()=>{
+
+  
+  /*setInterval(()=>{
     client.say(target,`To change the color of ${process.env.CHANNEL_WATCH}'s lights, just type !lights then the color you want them to change to. Go ahead, give it a try!`);
-  },60000);
+  },60000);*/
 }
 
 function setLights(h, s, l, target) {
   var group = 1;
-  h *= Math.round(65535/360);
+  h *= Math.round(65535 / 360);
   console.log(h);
   var url = `http://${process.env.BRIDGE_IP}/api/${process.env.HUE_UNAME}/groups/${group}/action`;
-  axios.put(url, { hue: h, sat: s, bri: l }).then((res) => {
-    
-  });
+  axios.put(url, { hue: h, sat: s, bri: l }).then((res) => {});
 }
